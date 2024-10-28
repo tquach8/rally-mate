a66c5a0bae23295141686737e5e3698a6442ff80
tquach8 <tanquach08@gmail.com>
2024-10-28 16:29:30 -0600

    Setup seed file

--- rallymate/src/app/api/signup/route.ts
+++ rallymate/src/app/api/signup/route.ts
@@ -1,3 +1,4 @@
+import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
@@ -16,10 +17,25 @@ export async function GET() {
   }
 }
 
-export async function POST() {
+export async function POST(request: Request) {
+  console.log("test");
   try {
     // const db = await connectToDatabase();
     // const events = await db.collection('schedules').find().toArray();
+    const { searchParams } = new URL(request.url);
+    debugger;
+    const petName = searchParams.get('petName');
+    const ownerName = searchParams.get('ownerName');
+
+    try {
+      if (!petName || !ownerName) throw new Error('Pet and owner names required');
+      await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
+    } catch (error) {
+      return NextResponse.json({ error }, { status: 500 });
+    }
+
+    const pets = await sql`SELECT * FROM Pets;`;
+    return NextResponse.json({ pets }, { status: 200 });
 
     const events = ["test"];
     return NextResponse.json(events, { status: 200 });
