888d469d855a0ba4f8514771e864105d7dc972a0
tquach8 <tanquach08@gmail.com>
2024-10-28 14:58:59 -0600

    add vercel stuff

--- rallymate/src/app/api/signup/route.js
+++ rallymate/src/app/api/signup/route.js
@@ -15,3 +15,19 @@ export async function GET() {
     );
   }
 }
+
+export async function POST() {
+  try {
+    // const db = await connectToDatabase();
+    // const events = await db.collection('schedules').find().toArray();
+
+    const events = ["test"];
+    return NextResponse.json(events, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching schedules:', error);
+    return NextResponse.json(
+      { error: 'An error occurred while fetching schedules.' },
+      { status: 500 }
+    );
+  }
+}
---
+++ rallymate/src/app/signup/form.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { useState } from "react";
+
+export default function Form() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
