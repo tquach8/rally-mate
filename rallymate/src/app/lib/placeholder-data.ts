1f32be2f80bbb541816ec2931ea3ec8a13987f8b
Karen Zhang <64809520+Karenzhang7717@users.noreply.github.com>
2024-10-29 11:50:55 -0600

    scheduling working

--- rallymate/src/app/lib/placeholder-data.ts
+++ rallymate/src/app/lib/placeholder-data.ts
@@ -6,5 +6,12 @@ const users = [
     password: "testtest",
   },
 ];
+const schedules = [
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442b",
+    user_id: "1",
+    availability_time: "2024-10-29",
+  },
+];
 
-export { users };
+export { users, schedules };
--- rallymate/src/app/seed/route.ts
+++ rallymate/src/app/seed/route.ts
@@ -2,33 +2,50 @@ import bcrypt from "bcrypt";
 import { db } from "@vercel/postgres";
 import { users } from "../lib/placeholder-data";
 import { User } from "../lib/definitions";
+import { schedules } from "../lib/placeholder-data";
+import { Schedule } from "../lib/definitions";
 
 const client = await db.connect();
+async function seedSchedule() {
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS schedules (
+      id SERIAL PRIMARY KEY,
+      user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+      availability_time TIMESTAMPTZ NOT NULL
+    );
+`;
+const insertedSchedule = await Promise.all(
+  schedules.map(async (schedule: Schedule) => {
+    return client.sql`
+      INSERT INTO schedules (user_id, availability_time)
+      VALUES (${schedule.user_id}, ${schedule.availability_time})
+      ON CONFLICT DO NOTHING;
+    `;
+  }),
+);
 
+return insertedSchedule;
+}
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await client.sql`
     CREATE TABLE IF NOT EXISTS users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email TEXT NOT NULL UNIQUE,
       password TEXT NOT NULL
     );
   `;
-  await client.sql`
-    CREATE TABLE IF NOT EXISTS schedules (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-      availability_time TIMESTAMPTZ NOT NULL
-    );
-  `;
+
+
+
   const insertedUsers = await Promise.all(
     users.map(async (user: User) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       return client.sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
+        INSERT INTO users (name, email, password)
+        VALUES (${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT DO NOTHING;
       `;
     }),
   );
@@ -40,6 +57,7 @@ export async function GET() {
   try {
     await client.sql`BEGIN`;
     await seedUsers();
+    await seedSchedule();
     await client.sql`COMMIT`;
 
     return Response.json({ message: 'Database seeded successfully' });
